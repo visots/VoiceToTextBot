ee72d0dd95fe9e82a4f539efe9c3b80a8b78ce1e
Alexey <visotskiy.alexey@gmail.com>
2023-03-12 21:40:46 +0700

    Сохранение данных

--- VoiceToTextBot/Controllers/InlineKeyboardController.cs
+++ VoiceToTextBot/Controllers/InlineKeyboardController.cs
@@ -5,23 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using VoiceToTextBot.Services;
 
 namespace VoiceToTextBot.Controllers
 {
     internal class InlineKeyboardController
     {
+        private readonly IStorage _memoryStorage;
         private readonly ITelegramBotClient _telegramBotClient;
 
-        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
         {
             this._telegramBotClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
         }
 
-        public async Task Handle(CallbackQuery? callbackQuery,CancellationToken ct)
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
         {
-            Console.WriteLine($"Контроллер {GetType().Name}");
+            Console.WriteLine("Контроллер " + GetType().Name);
 
-            await _telegramBotClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку {callbackQuery.Data}", cancellationToken: ct);
+            if (callbackQuery?.Data == null)
+                return;
+
+            _memoryStorage.GetSession(callbackQuery.From.Id).LanguageCode = callbackQuery.Data;
+
+            string languageText = callbackQuery.Data switch
+            {
+                "ru" => "Русский",
+                "en" => "Английский",
+                _ => String.Empty
+            };
+
+            string msg = $"<b>Язык аудио - {languageText}.{Environment.NewLine}</b>" +
+                            $"{Environment.NewLine}Можно поменять в главном меню.";
+
+            await _telegramBotClient.SendTextMessageAsync(callbackQuery.From.Id,msg, cancellationToken: ct, parseMode: ParseMode.Html);
         }
     }
 }
